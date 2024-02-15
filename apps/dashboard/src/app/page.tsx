0db737ccf1e24ed622395e23fcddcf7717e44912
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-16 01:56:52 +0530

    WIP on workspace:UI its still not outputing the tailwindcss styles

---
+++ apps/dashboard/src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from "@repo/ui";
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="flex items-center justify-center w-screen h-screen text-white bg-red-500">
+      <div className="text-center">
+        <div className="my-3 text-3xl">Home Page</div>
+        <Button type="submit" className="w-20 h-auto disabled:opacity-100">
+          Login
+        </Button>
+      </div>
+    </main>
+  );
+}
