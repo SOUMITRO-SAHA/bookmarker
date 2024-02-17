4a43c4b7047bc96857d4988f69e59958aeb53b58
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-17 23:17:30 +0530

    All Apis shited to  format

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,10 +1,3 @@
-"use client";
-import { api } from "@/lib/trpc";
-import { useEffect } from "react";
-
 export default function Page() {
-  const test = api.getUsers.query();
-  console.log(test);
-
   return <main>Home Page</main>;
 }
