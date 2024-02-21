8e604cfd4d477f85994cf2f4be0361ff4c599adf
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-21 15:25:29 +0530

    Dependencies Updated

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -3,25 +3,40 @@ import DiscordProvider from "next-auth/providers/discord";
 
 import { db } from "@repo/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { WEBAPP_URL } from "@repo/lib";
 
 export function isNumber(n: string) {
   return !isNaN(parseFloat(n)) && !isNaN(+n);
 }
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(db),
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
+    maxAge: 3 * 30 * 24 * 60 * 60, // 30 days
+    updateAge: 7 * 24 * 60 * 60, // 24 hours
   },
-  cookies: defaultCookies(WEBAPP_URL?.startsWith("https://")),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
       clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     }),
   ],
+  pages: {
+    signIn: "/auth/sign-in",
+    signOut: "/auth/sign-out",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    newUser: "/auth/register",
+  },
   callbacks: {
+    signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    jwt({ token, user, account, profile, isNewUser }) {
+      return token;
+    },
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
