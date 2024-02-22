344da67490b04433b92f3fa2e0c48427cc778f55
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-22 16:26:07 +0530

    UI Updated

--- apps/dashboard/src/common/Input/PasswordField.tsx
+++ apps/dashboard/src/common/Input/PasswordField.tsx
@@ -1,190 +1,10 @@
 import * as React from "react";
-
-import { Input } from "../ui/input";
-import { InputFieldProps } from "./types";
-import { Button } from "../Button";
-import { Tooltip } from "../tooltip/Tooltip";
-import { Eye, EyeOff, X } from "lucide-react";
-import { HintsOrErrors } from "./HintOrErrors";
+import { InputField } from "./InputField";
+import Tooltip from "../tooltip/Tooltip";
 import { cn } from "@/lib/utils";
-import { Skeleton } from "../ui/skeleton";
-import { Label } from "./Label";
-
-type AddonProps = {
-  children: React.ReactNode;
-  isFilled?: boolean;
-  className?: string;
-  error?: boolean;
-  onClickAddon?: () => void;
-};
-
-const Addon = ({
-  isFilled,
-  children,
-  className,
-  error,
-  onClickAddon,
-}: AddonProps) => (
-  <div
-    onClick={onClickAddon && onClickAddon}
-    className={cn(
-      "addon-wrapper border-default [input:hover_+_&]:border-emphasis [input:hover_+_&]:border-l-default [&:has(+_input:hover)]:border-emphasis [&:has(+_input:hover)]:border-r-default h-10 border px-3",
-      isFilled && "bg-subtle",
-      onClickAddon && "cursor-pointer disabled:hover:cursor-not-allowed",
-      className
-    )}
-  >
-    <div
-      className={cn(
-        "min-h-9 flex flex-col justify-center text-sm leading-7",
-        error ? "text-error" : "text-default"
-      )}
-    >
-      <span className="flex whitespace-nowrap">{children}</span>
-    </div>
-  </div>
-);
-
-export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
-  function InputField(props, ref) {
-    const id = React.useId();
-    const name = props.name || "";
-    const {
-      label = name,
-      labelProps,
-      labelClassName,
-      disabled,
-      LockedIcon,
-      placeholder = `${name}_placeholder`,
-      className,
-      addOnLeading,
-      addOnSuffix,
-      addOnFilled = true,
-      addOnClassname,
-      inputIsFullWidth,
-      hint,
-      type,
-      hintErrors,
-      labelSrOnly,
-      containerClassName,
-      readOnly,
-      showAsteriskIndicator,
-      onClickAddon,
-      ...passThrough
-    } = props;
-
-    const [inputValue, setInputValue] = React.useState<string>("");
-
-    return (
-      <div className={cn(containerClassName)}>
-        {!!name && (
-          <Skeleton
-            as={Label}
-            htmlFor={id}
-            loadingClassName="w-16"
-            {...labelProps}
-            className={cn(
-              labelClassName,
-              labelSrOnly && "sr-only",
-              props.error && "text-error"
-            )}
-          >
-            {label}
-            {showAsteriskIndicator && !readOnly && passThrough.required ? (
-              <span className="text-default ml-1 font-medium">*</span>
-            ) : null}
-            {LockedIcon}
-          </Skeleton>
-        )}
-        {addOnLeading || addOnSuffix ? (
-          <div
-            dir="ltr"
-            className="focus-within:ring-primary group relative mb-1 flex items-center rounded-md focus-within:outline-none focus-within:ring-2"
-          >
-            {addOnLeading && (
-              <Addon
-                isFilled={addOnFilled}
-                className={cn(
-                  "ltr:rounded-l-md rtl:rounded-r-md",
-                  addOnClassname
-                )}
-              >
-                {addOnLeading}
-              </Addon>
-            )}
-            <Input
-              id={id}
-              type={type}
-              placeholder={placeholder}
-              className={cn(
-                className,
-                "disabled:bg-muted disabled:hover:border-primary-foregrounddisabled:cursor-not-allowed",
-                addOnLeading && "rounded-l-none border-l-0",
-                addOnSuffix && "rounded-r-none border-r-0",
-                type === "search" && "pr-8",
-                "!my-0 !ring-0"
-              )}
-              {...passThrough}
-              {...(type == "search" && {
-                onChange: (e) => {
-                  setInputValue(e.target.value);
-                  props.onChange && props.onChange(e);
-                },
-                value: inputValue,
-              })}
-              disabled={readOnly || disabled}
-              ref={ref}
-            />
-            {addOnSuffix && (
-              <Addon
-                onClickAddon={onClickAddon}
-                isFilled={addOnFilled}
-                className={cn(
-                  "ltr:rounded-r-md rtl:rounded-l-md",
-                  addOnClassname
-                )}
-              >
-                {addOnSuffix}
-              </Addon>
-            )}
-            {type === "search" && inputValue?.toString().length > 0 && (
-              <X
-                className="text-subtle absolute top-2.5 h-4 w-4 cursor-pointer ltr:right-2 rtl:left-2"
-                onClick={(e) => {
-                  setInputValue("");
-                  props.onChange &&
-                    props.onChange(
-                      e as unknown as React.ChangeEvent<HTMLInputElement>
-                    );
-                }}
-              />
-            )}
-          </div>
-        ) : (
-          <Input
-            id={id}
-            type={type}
-            placeholder={placeholder}
-            className={cn(
-              className,
-              "disabled:bg-subtle disabled:hover:border-subtle disabled:cursor-not-allowed"
-            )}
-            {...passThrough}
-            readOnly={readOnly}
-            ref={ref}
-            disabled={readOnly || disabled}
-          />
-        )}
-        <HintsOrErrors hintErrors={hintErrors} fieldName={name} />
-        {hint && (
-          <div className="text-default mt-2 flex items-center text-sm">
-            {hint}
-          </div>
-        )}
-      </div>
-    );
-  }
-);
+import { Eye, EyeOff } from "lucide-react";
+import { Button } from "../Button";
+import { InputFieldProps } from "./types";
 
 export const PasswordField = React.forwardRef<
   HTMLInputElement,
@@ -204,17 +24,14 @@ export const PasswordField = React.forwardRef<
       ref={ref}
       {...props}
       className={cn(
-        "addon-wrapper placeholder:leading-8 mb-0 ltr:border-r-0 ltr:pr-10 rtl:border-l-0 rtl:pl-10",
+        "addon-wrapper mb-0 ltr:border-r-0 ltr:pr-10 rtl:border-l-0 rtl:pl-10",
         props.className
       )}
       addOnFilled={false}
       addOnSuffix={
-        <Tooltip
-          content={textLabel}
-          className="bg-primary text-primary-foreground"
-        >
+        <Tooltip content={textLabel}>
           <Button
-            className="text-emphasis w-6 p-1 h-10"
+            className="w-6 h-10 p-1 text-emphasis"
             variant={"ghost"}
             onClick={() => toggleIsPasswordVisible()}
           >
