a1fd82223009f81951d06ef5fc7197f3ab261b5a
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-24 00:53:47 +0530

    Shared Package Added

---
+++ packages/shared/schemas/auth.ts
@@ -0,0 +1,24 @@
+import * as z from "zod";
+
+export const PasswordSchema = z
+  .string()
+  .min(8)
+  .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/, {
+    message:
+      "Password must contain at least one lowercase letter, one uppercase letter, and one digit",
+  });
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email!!!",
+  }),
+  password: PasswordSchema,
+});
+
+export const RegisterSchema = z.object({
+  username: z.string().min(3).max(50),
+  email: z.string().email({
+    message: "Invalid email!!!",
+  }),
+  password: PasswordSchema,
+});
