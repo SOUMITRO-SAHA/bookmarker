f608fc7882d556d589d100dad080b36663be1aed
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-24 21:18:39 +0530

    Button UI Complete

--- apps/dashboard/src/common/Button/button.tsx
+++ apps/dashboard/src/common/Button/button.tsx
@@ -1,8 +1,7 @@
-import * as React from "react";
+import { cn } from "@/lib/utils";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
-import { cn } from "@/lib/utils";
+import * as React from "react";
 import Loader from "../Loader/Loader";
 
 const buttonVariants = cva(
@@ -18,7 +17,7 @@ const buttonVariants = cva(
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-accent",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
@@ -36,36 +35,58 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends React.ButtonHTMLAttributes<HTMLButtonElement | HTMLDivElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  isLoading?: boolean;
+  loading?: boolean;
+  asDiv?: boolean;
+  suffixIcon?: React.ReactNode;
+  prefixIcon?: React.ReactNode;
 }
+const Button = React.forwardRef<
+  HTMLButtonElement | HTMLDivElement,
+  ButtonProps
+>((props: ButtonProps, ref) => {
+  const {
+    className,
+    variant,
+    size,
+    asDiv = false,
+    asChild = false,
+    loading = false,
+    prefixIcon,
+    suffixIcon,
+  } = props;
+  const Comp = asChild ? Slot : asDiv ? "div" : "button";
+  const disabled = props.disabled || loading;
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    { className, variant, size, asChild = false, isLoading = false, ...props },
-    ref
-  ) => {
-    const Comp = asChild ? Slot : "button";
-
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {isLoading ? (
-          <div className="flex items-center justify-center gap-2">
-            <Loader size={15} color={variant === "default" ? "#556671" : ""} />
-            <span>{props.title}</span>
-          </div>
-        ) : (
-          props.children
-        )}
-      </Comp>
-    );
-  }
-);
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref as React.RefObject<HTMLButtonElement | HTMLDivElement | any>}
+      {...props}
+      disabled={disabled}
+    >
+      {loading ? (
+        <div className="flex items-center justify-center gap-2">
+          <Loader size={15} color={variant === "default" ? "#556671" : ""} />
+          <span aria-disabled>{props.children}</span>
+        </div>
+      ) : (
+        <div
+          className={cn(
+            "flex items-center justify-center",
+            prefixIcon ? "gap-2" : "",
+            suffixIcon ? "gap-2" : ""
+          )}
+        >
+          {prefixIcon && <>{prefixIcon}</>}
+          {props.children}
+          {suffixIcon && <>{suffixIcon}</>}
+        </div>
+      )}
+    </Comp>
+  );
+});
 Button.displayName = "Button";
 export { Button, buttonVariants };
