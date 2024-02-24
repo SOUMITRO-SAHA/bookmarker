f49566baafa6be77eb4af00111b0c97f6950bbd0
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-24 21:20:53 +0530

    WIP on Login Form

---
+++ apps/dashboard/src/components/auth/Login/LoginForm.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { Button } from "@/common/Button";
+import { InputField } from "@/common/Input/InputField";
+import { PasswordField } from "@/common/Input/PasswordField";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/common/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@repo/shared";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+interface LoginFormProps {
+  //Props
+}
+
+const LoginForm: React.FC<LoginFormProps> = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof LoginSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <InputField
+                    placeholder="i.e. john.demo@emample.com"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordField {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;
