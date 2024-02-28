19aa344b3dd91f39ecbfd343fb98663908eba7ce
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-28 23:39:20 +0530

    Imports Page | Created

---
+++ apps/dashboard/src/app/(protected)/settings/imports/page.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent } from "@/common/ui/card";
+import React from "react";
+
+interface ImportsPageProps {
+  //Props
+}
+
+const ImportsPage: React.FC<ImportsPageProps> = () => {
+  return (
+    <main className="flex items-center justify-center w-full h-full">
+      <Card>
+        <CardContent>Working On this Page</CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default ImportsPage;
