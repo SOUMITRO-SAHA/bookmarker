298e498889b5e80747acd1b47f15d252b7c60151
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-02-29 00:50:01 +0530

    Re-Factor

--- apps/dashboard/src/app/(protected)/dashboard/layout.tsx
+++ apps/dashboard/src/app/(protected)/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import Sidebar from "@/components/SideBar/Sidebar";
 import { Metadata } from "next";
 const metadata: Metadata = {
-  title: "DashboardLayout",
+  title: "Dashboard | Bookmarker",
   description: "",
 };
 export default function DashboardLayout({
@@ -11,12 +11,12 @@ export default function DashboardLayout({
 }): JSX.Element {
   return (
     <main className="flex w-screen h-screen">
-      <section className="w-[15%]">
+      {/* Sidebar */}
+      <section>
         <Sidebar />
       </section>
-      <section className="bg-subtle p-10 w-[calc(100%-15%)]">
-        {children}
-      </section>
+      {/* Body */}
+      <section className="w-full p-10 bg-subtle">{children}</section>
     </main>
   );
 }
