ffc9ec4edc010fb1feff7f5709ee3bc34f681525
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-05 02:11:45 +0530

    Login Action setup

--- apps/dashboard/src/actions/login.ts
+++ apps/dashboard/src/actions/login.ts
@@ -1,1 +1,15 @@
-"use server"
+"use server";
+import { LoginSchema } from "@repo/shared";
+import * as z from "zod";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid Fields!!!" };
+  }
+  try {
+    const { email, password } = values;
+    console.log(email, password);
+    return { success: "Successfully Logged in!" };
+  } catch (error) {}
+};
