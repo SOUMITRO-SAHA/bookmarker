d1e8ad7768b04b10f5c66824daa4be1b0e847549
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-05 02:10:45 +0530

    Defining the Routes and Constants

---
+++ apps/dashboard/src/routes.ts
@@ -0,0 +1,9 @@
+export const publicRoutes = ["/"];
+
+export const protectedRoutes = [];
+
+export const authRoutes = ["/auth/login", "/auth/signup"];
+
+export const APIAUTHPREFIX = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
