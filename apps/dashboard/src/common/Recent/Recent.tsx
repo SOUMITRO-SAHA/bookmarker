8658be5e8296ac23f26ff9e64d592d9aa7c690be
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-05 12:31:44 +0530

    Recent | Last open | Last Activity component Created

---
+++ apps/dashboard/src/common/Recent/Recent.tsx
@@ -0,0 +1,14 @@
+import { H3 } from "@/components/General";
+import React from "react";
+
+interface RecentProps {
+  //Props
+}
+
+export const Recent: React.FC<RecentProps> = () => {
+  return (
+    <div>
+      <H3>Recent</H3>
+    </div>
+  );
+};
