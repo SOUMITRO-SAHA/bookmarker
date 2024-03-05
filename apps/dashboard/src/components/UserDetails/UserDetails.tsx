4d827857708ec6f4032714e1b5adcbf2186c8ec6
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-05 12:47:32 +0530

    UI Updated

--- apps/dashboard/src/components/UserDetails/UserDetails.tsx
+++ apps/dashboard/src/components/UserDetails/UserDetails.tsx
@@ -8,7 +8,7 @@ interface UserDetailsProps {
 
 const UserDetails: React.FC<UserDetailsProps> = () => {
   return (
-    <div className="flex items-center gap-3">
+    <div className="flex items-center w-full gap-3">
       {/* Avatar */}
       <div>
         <Avatar>
@@ -19,17 +19,17 @@ const UserDetails: React.FC<UserDetailsProps> = () => {
 
       {/* Details */}
       <div className="flex flex-col justify-center">
-        <div className="dark:text-slate-200 text-start">
+        <div className="text-sm dark:text-slate-200 text-start">
           {userDetails.username}
         </div>
-        <div className="flex items-center gap-1 text-xs text-slate-500">
+        <div className="flex items-center gap-1 text-xs group text-slate-500">
           {/* Membership Info */}
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1 text-xs group-hover:hidden">
             <span>{userDetails.membership}</span>
             <span>Plan</span>
           </div>
           {/* Last Online */}
-          <div className="flex items-center gap-1">
+          <div className="items-center hidden gap-1 text-sx group-hover:flex">
             <span className="w-2 h-2 bg-green-400 rounded-full" />
             <span>{userDetails.lastOnline}</span>
           </div>
