c37554d769c90e3194a07b8a4e4860f3ceda3342
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-05 12:28:24 +0530

    Notification Component Created

---
+++ apps/dashboard/src/common/Notification/Notification.tsx
@@ -0,0 +1,14 @@
+import { H3 } from "@/components/General";
+import React from "react";
+
+interface NotificationProps {
+  //Props
+}
+
+export const Notification: React.FC<NotificationProps> = () => {
+  return (
+    <div>
+      <H3>Notification</H3>
+    </div>
+  );
+};
