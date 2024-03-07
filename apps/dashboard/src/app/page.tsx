0a24eaadcdb946a9a798fe05b475e637e6dbb396
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-07 23:50:04 +0530

    Navbar Added in the Landing Page

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <main>Home Page</main>;
+import { auth } from "@/auth";
+import Navbar from "@/components/LandingPage/Navbar/Navbar";
+
+export default async function Main() {
+  const session = await auth();
+  console.log(session);
+  return (
+    <main>
+      {/* Navbar */}
+      <Navbar />
+    </main>
+  );
 }
