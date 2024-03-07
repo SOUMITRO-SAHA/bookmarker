19743840db8f706961c2764ed6950921cd8df53c
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-07 23:45:12 +0530

    UI Updated

--- apps/dashboard/src/app/error.tsx
+++ apps/dashboard/src/app/error.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Button } from "@/common/Button";
+import { PageTitle, TitleHeader } from "@/components/General";
 import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 interface errorProps {
@@ -10,16 +12,27 @@ interface errorProps {
 
 const error: React.FC<errorProps> = () => {
   return (
-    <main className="bg-background">
-      <div className="uppercase">Error 404</div>
-      <div className="text-3xl capitalize">This page does not exist.</div>
-      <div>Check for spelling mistakes or go back to the previous page.</div>
-      <Button>
-        <span>
-          <ArrowLeft />
-        </span>
-        <span>Go Back to Home</span>
-      </Button>
+    <main className="flex items-center justify-center w-screen h-screen bg-background">
+      <section className="flex flex-col justify-center">
+        <TitleHeader
+          title="Error 404"
+          subtitle="This page does not exist."
+          description="Check for spelling mistakes or go back to the previous page."
+        />
+        <Button
+          variant={"link"}
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          <Link href={"/"} className="flex items-center gap-3">
+            <span>
+              <ArrowLeft />
+            </span>
+            <span>Go Back to Home</span>
+          </Link>
+        </Button>
+      </section>
     </main>
   );
 };
