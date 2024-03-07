d90219c214b94eccfe75d8f3ed5bd093322943b1
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-07 23:44:07 +0530

    Re-Factor

--- apps/dashboard/src/auth.ts
+++ apps/dashboard/src/auth.ts
@@ -2,12 +2,14 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { db } from "@repo/db";
+import { env } from "@/config/env";
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
+  secret: env.AUTH_SECRET,
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
-}) as any; // TODO: Resove this issue `auth` is not recognized
+}) as any;
