02e8f0d1390494ea260c3a1f0f2a585a22ee1d9e
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-10 00:25:56 +0530

    `.env` updates

--- apps/dashboard/src/config/env.ts
+++ apps/dashboard/src/config/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   AUTH_SECRET: process.env.AUTH_SECRET,
   AUTH_EXPIRY: process.env.AUTH_EXPIRY,
-  GOOGLE_ID: process.env.GOOGLE_ID || "",
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || "",
+  GOOGLE_ID: process.env.AUTH_GOOGLE_ID || "",
+  GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET || "",
 };
