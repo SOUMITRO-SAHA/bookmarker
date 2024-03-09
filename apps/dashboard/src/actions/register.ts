a0470ee03be830df7153aa4404c75b816a8bc13e
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-10 00:23:16 +0530

    Successfully Created the User in the DB, Errors Handled

--- apps/dashboard/src/actions/register.ts
+++ apps/dashboard/src/actions/register.ts
@@ -4,21 +4,31 @@ import { encryptPassword } from "@repo/lib";
 import { RegisterSchema } from "@repo/shared";
 import * as z from "zod";
 
-export const register = async (values: z.infer<typeof RegisterSchema>) => {
+type Register = {
+  success: boolean;
+  message: string;
+  data?: any;
+};
+
+export const register = async (
+  values: z.infer<typeof RegisterSchema>
+): Promise<Register | undefined> => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields) {
-    return { error: "Invalid Field!" };
+    return { success: false, message: "Invalid Field!" };
   }
+
   try {
     const { username, email, password } = values;
     const hashedPassword = await encryptPassword(password);
+
     // Checking for existing user with same email
     const existingUser = await db.user.findUnique({
-      where: { email },
+      where: { email: email },
     });
 
     if (existingUser) {
-      return { error: "Email already in user!!!" };
+      return { success: false, message: "Email already in use!!!" };
     }
 
     // Creating new User
@@ -33,7 +43,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         userId: newUser.id,
       },
     });
-    // TODO: Send a verification email to the user
-    return { success: "Successfully Registered!!!" };
-  } catch (error) {}
+    return { success: true, message: "Successfully Registered!!!" };
+  } catch (error) {
+    if (error instanceof Error)
+      return { success: false, message: "Error: " + error.message };
+  }
 };
