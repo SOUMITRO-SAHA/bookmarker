d3174f62cc1848e3c9be007cab714dce3e1e8f0b
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2024-03-27 00:57:06 +0530

    Updating the Imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/library.js"],
+  extends: ["@bookmarker/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   ignorePatterns: ["apps/**", "packages/**"],
   parserOptions: {
